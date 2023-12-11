d6b969159f5a062e8bf582729f8b921ba9900c65
taigi100 <contact@taigi100.com>
2023-12-11 23:00:04 +0200

    Debug CI/CD

--- src/entities/zoomable/zoomable.gd
+++ src/entities/zoomable/zoomable.gd
@@ -9,5 +9,6 @@ func _on_mouse_entered() -> void:
 
 func _on_input_event(camera:Node, event:InputEvent, _position:Vector3, _normal:Vector3, _shape_idx:int) -> void:
 	if event is InputEventMouseButton and (event as InputEventMouseButton).pressed == true:
+		print("Just testing CI/CD is still ok :)")
 		(camera as Camera3D).position = origin.position; 
 		(camera as Camera3D).look_at(lookat.global_position); 
